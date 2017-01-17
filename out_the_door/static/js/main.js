24bf49f2c04219b114dfd35b27e906525522b4ad
smedinacorey <s.medinacorey@gmail.com>
2017-01-17 21:11:37 +0000

    added functionality and ajax call in main js

--- out_the_door/static/js/main.js
+++ out_the_door/static/js/main.js
@@ -1,3 +1,56 @@
+var outTheDoor = function() {
+    
+    this.profileList = $("#profile-list");
+    // Compile the profile list template from the HTML file
+    this.profileListTemplate = Handlebars.compile($("#profile-list-template").html());
+    
+    this.profiles = [];
+    // Get the current list of uploaded profiles
+    this.getProfiles();
+};
+
+outTheDoor.prototype.getProfiles = function() {
+    // Make a get request to list all of the profiles
+    var ajax = $.ajax('/api/profiles', {
+        type: 'GET',
+        dataType: 'json'
+    });
+    
+    // add arguments here
+    ajax.done();
+    ajax.fail();
+};
+
+outTheDoor.prototype.onGetProfilesDone = function(data) {
+    // Update the profiles array, and update the user interface
+    this.profiles = data;
+    this.updateProfileView();
+};
+
+outTheDoor.prototype.updateProfileView = function() {
+    // Render the handlebars template for the profile list, and insert it into
+    // the DOM
+    var context = {
+        profiles: this.profiles
+    };
+
+    var profileList = $(this.profileListTemplate(context));
+    this.profileList.replaceWith(profileList);
+    this.profileList = profileList;
+};
+
+outTheDoor.prototype.onFail = function(what, event) {
+    // Called when an AJAX call fails
+    console.error(what, "failed: ", event.statusText);
+};
+
+$(document).ready(function() {
+    window.app = new outTheDoor();
+});
+
+
+
+
 // <form method="POST" role="form">
 //     <div class="form-group">
 //         <label for="title">Caption</label>
