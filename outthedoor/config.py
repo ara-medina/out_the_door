b2a5c97c1ac9d2084c5c728ad365508b942c71f2
smedinacorey <s.medinacorey@gmail.com>
2017-04-02 22:19:33 +0000

    removed database_uri paths

--- outthedoor/config.py
+++ outthedoor/config.py
@@ -1,14 +1,14 @@
 import os
 
 class DevelopmentConfig(object):
-    DATABASE_URI = "postgresql://ubuntu:thinkful@localhost:5432/outthedoor"
+    # DATABASE_URI = "postgresql://ubuntu:thinkful@localhost:5432/outthedoor"
     DEBUG = True
     UPLOAD_FOLDER = "uploads"
     ALLOWED_EXTENSIONS = set(['png', 'jpg', 'jpeg'])
     SECRET_KEY = os.environ.get("OUTTHEDOOR_SECRET_KEY", os.urandom(12))
 
 class TestingConfig(object):
-    DATABASE_URI = "postgresql://ubuntu:thinkful@localhost:5432/outthedoor-test"
+    # DATABASE_URI = "postgresql://ubuntu:thinkful@localhost:5432/outthedoor-test"
     DEBUG = True
     UPLOAD_FOLDER = "test-uploads"
     ALLOWED_EXTENSIONS = set(['png', 'jpg', 'jpeg'])
